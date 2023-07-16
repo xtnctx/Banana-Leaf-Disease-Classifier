563e277496932f54b1e33b3ad342de2e0e69c3fb
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-07-16 20:46:42 +0800

    Add files via upload

--- Software/Widgets/toolBar.py
+++ Software/Widgets/toolBar.py
@@ -20,7 +20,7 @@ class ToolBar(QtWidgets.QToolBar):
         self.setStyleSheet("QToolBar{spacing: 20px;}")
         self.setFont(font)
         
-        self.setOrientation(QtCore.Qt.Vertical)
+        # self.setOrientation(QtCore.Qt.Vertical)
         self.setIconSize(QtCore.QSize(30, 25))
         self.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
 
@@ -33,14 +33,14 @@ class ToolBar(QtWidgets.QToolBar):
         self.actionFolder.setIcon(icon)
         self.addAction(self.actionFolder)
 
-        # Results
-        self.actionResults = QtWidgets.QAction(parent)
-        self.actionResults.setFont(font)
-        self.actionResults.setText("Results")
+        # Analytics
+        self.actionAnalytics = QtWidgets.QAction(parent)
+        self.actionAnalytics.setFont(font)
+        self.actionAnalytics.setText("Analytics")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("./svgIcons/Nx5gxW0N_V1z/mbri-growing-chart.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.actionResults.setIcon(icon)
-        self.addAction(self.actionResults)
+        self.actionAnalytics.setIcon(icon)
+        self.addAction(self.actionAnalytics)
 
         # Camera
         self.actionCamera = QtWidgets.QAction(parent)
