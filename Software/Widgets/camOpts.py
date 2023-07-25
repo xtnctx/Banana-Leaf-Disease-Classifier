8711b5d30a54c2e7b897940f443be07e727d6c96
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-07-25 23:45:20 +0800

    Func. get_available_cameras added as staticmethod

--- Software/Widgets/camOpts.py
+++ Software/Widgets/camOpts.py
@@ -1,21 +1,11 @@
 from PyQt5 import QtWidgets, QtCore
 from pygrabber.dshow_graph import FilterGraph
 
-def get_available_cameras():
-
-    devices = FilterGraph().get_input_devices()
-
-    available_cameras = {}
-
-    for device_index, device_name in enumerate(devices):
-        available_cameras[device_index] = device_name
-
-    return available_cameras
 
 class CamOptions(QtWidgets.QWidget):
     camUsed = QtCore.pyqtSignal(int)
 
-    def __init__(self):
+    def __init__(self, devices:list):
         super().__init__()
         vbox = QtWidgets.QVBoxLayout(self)
 
@@ -23,8 +13,7 @@ class CamOptions(QtWidgets.QWidget):
         vbox.addWidget(self.name)
 
         self.comboBox = QtWidgets.QComboBox()
-        cam_list = ["Cam1", "Cam2", "Cam3", "Cam4"]
-        self.comboBox.addItems(cam_list)
+        self.comboBox.addItems(devices)
         self.comboBox.currentIndexChanged.connect(self.on_combobox_changed)
         vbox.addWidget(self.comboBox)
 
@@ -39,3 +28,13 @@ class CamOptions(QtWidgets.QWidget):
     
     def on_combobox_changed(self, index):
         self.camUsed.emit(index)
+    
+    @staticmethod
+    def get_available_cameras():
+        devices = FilterGraph().get_input_devices()
+        available_cameras = {}
+
+        for device_index, device_name in enumerate(devices):
+            available_cameras[device_index] = device_name
+
+        return available_cameras
