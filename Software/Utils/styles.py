b969e51ebcd16321ccdf52b63417eccc520e3828
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-08-27 23:04:06 +0800

    Add files via upload

---
+++ Software/Utils/styles.py
@@ -0,0 +1,7 @@
+slider = '''
+    QSlider::handle:horizontal:hover {
+        background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #fff, stop:1 #ddd);
+        border: 1px solid #444;
+        border-radius: 4px;
+    }
+'''
