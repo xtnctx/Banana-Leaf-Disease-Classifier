0abd25f0008b998c2524aa7d0ea92f5bd7ee96f7
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-08-29 21:50:12 +0800

    Add files via upload

--- Software/Utils/style.py
+++ Software/Utils/style.py
@@ -1,7 +1,12 @@
+from PyQt5 import QtGui
 from . import system
 
 color = system.getAccentColor()
 table_item_selected_color = f'rgb({color[0]}, {color[1]}, {color[2]})'
+windowsAccentColor =  QtGui.QColor(color[0], color[1], color[2])
+
+right_box = f'border: 1px solid {table_item_selected_color}; color: #000000'
+wrong_box = 'border: 1px solid #C5C5C5; color: #C5C5C5'
 
 slider = '''
     QSlider::handle:horizontal:hover {
@@ -22,5 +27,3 @@ table = '''
         background-color: rgba(255, 255, 255, 128);
     }
 ''' % (table_item_selected_color)
-
-
